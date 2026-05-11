598e479cf0dbb2308a9a5afc17ed77f2d64751ab
Elazar <elazer1996@gmail.com>
2026-05-11 15:12:06 +0300

    Finished homework 6

---
+++ lesson6/02-type-conversions-&-typeof.js
@@ -0,0 +1,10 @@
+let a = "15";
+let b = 10;
+let c = "2";
+let sum = Number(a) + b;
+console.log("sum:", sum);
+console.log("type:", typeof sum);
+console.log("a * c:", a * c);
+
+
+//כי באופרטור כפל javaScript יודע שזה אפשרי רק במספרים לכן הוא ממיר ישירות למספר מה שלא נכון לגבי אופרטור + שיכול לשמש גם לשירשור
